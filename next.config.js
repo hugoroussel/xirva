f85806b3cdb553fa730ec2f09ac9ab0182977898
Hugo Roussel <hugo.roussel@epfl.ch>
2021-08-07 20:10:49 +0200

    Ignore eslint during deploys

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  eslint: {
+    // Warning: Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   trailingSlash: true,
   exportPathMap() {
     return {
