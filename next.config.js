69296b2a069f110040e9313ed5ca1669651824d6
Hugo Roussel <hugo.roussel@epfl.ch>
2021-08-15 01:32:10 +0200

    Add new path

--- next.config.js
+++ next.config.js
@@ -23,6 +23,7 @@ module.exports = {
       '/stat': { page: '/stat' },
       '/about': { page: '/about' },
       '/upload': { page: '/upload' },
+      '/new': { page: '/new' },
     };
     return paths;
   },
