3fec1201efd9f7c4533e9ee081c62a2d12661a11
Hugo Roussel <hugo.roussel@epfl.ch>
2021-08-14 19:38:02 +0200

    Add favicons

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,11 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>Xirva | Upload your scientific papers on the Decentralised Web</title>
         <meta name="Xirva | Upload your scientific papers on the Decentralised Web" content="Xirva " />
-        <link rel="icon" href="/public/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <Component {...pageProps} />
     </>
